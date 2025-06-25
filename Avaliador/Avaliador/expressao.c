9527eb258b92ba1e5b479a22607ceb81a0ab6f6d
Pedro Oliveira <183723342+pedro-oliveira-ucb@users.noreply.github.com>
2025-06-25 12:34:40 -0300

    feat: Initial commit for expression conversion and evaluation
    -Added main.c, expressao.c, and expressao.h with function stubs.
    -Implemented initial structure for expression conversion and evaluation.
    -Updated .gitignore, project, and filter files to include new source files and configurations..

---
+++ Avaliador/Avaliador/expressao.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+#include "expressao.h"
+
+char * getFormaInFixa( char * Str )
+{
+	return NULL;
+}
+
+char * getFormaPosFixa( char * Str )
+{
+	return NULL;
+}
+
+float getValorPosFixa( char * StrPosFixa )
+{
+	return 0.0f;
+}
+
+float getValorInFixa( char * StrInFixa )
+{
+	return 0.0f;
+}
